0d8b3bdb609ac56d1be2dd5a080a7182d3fd2004
xinghyfish <xinghy.fish@qq.com>
2022-06-05 23:55:08 +0800

    完成签到功能，但是python相对路径错误引发了非常诡异的问题，明天再说

--- client/controller/websocket.py
+++ client/controller/websocket.py
@@ -1,3 +1,5 @@
+import sys
+from datetime import datetime
 import os.path
 import time
 from abc import ABC
@@ -16,8 +18,9 @@ class ChatHandler(WebSocketHandler, ABC):
     def message_file_path(self, user) -> str:
         teacherName = self.get_argument("teacherName")
         courseName = self.get_argument("courseName")
-        return 'history//%s//%s//%s.txt' % \
-               (teacherName, courseName, self.pools[teacherName][courseName].user_dict[user])
+        return "hello.txt"
+        # return './resources//history//%s//%s//%s.txt' % \
+        #        (teacherName, courseName, self.pools[teacherName][courseName].user_dict[user])
 
     def open(self):
         """When connect is built, add websocket object to users set"""
@@ -41,11 +44,11 @@ class ChatHandler(WebSocketHandler, ABC):
         if message.endswith('**#*'):
             # account information ends with '**#*'
             self.pools[teacherName][courseName].user_dict[self] = message[:-4]
-            if os.path.exists(filepath):
-                fp = open(filepath, 'r', encoding='utf-8')
-                history = fp.read()
-                fp.close()
-                self.write_message(history)
+            # if os.path.exists(filepath):
+            #     fp = open(filepath, 'a', encoding='utf-8')
+            #     history = fp.read()
+            #     fp.close()
+            #     self.write_message(history)
             send_message = tm + '【' + self.pools[teacherName][courseName].user_dict[self] + '】已进入直播间！' + '\n'
             for user in self.pools[teacherName][courseName].users:
                 filepath = self.message_file_path(user)
@@ -68,7 +71,7 @@ class ChatHandler(WebSocketHandler, ABC):
         filepath = self.message_file_path(self)
         action_time = time.asctime(time.localtime(time.time()))[-13:-4]
         send_message = action_time + '【' + self.pools[teacherName][courseName].user_dict[self] + '】已离开直播间'
-        fp = open(filepath, 'a', encoding='utf')
+        fp = open(file=filepath, mode='a', encoding='utf-8')
         fp.write(send_message)
         fp.close()
         for user in self.pools[teacherName][courseName].users:
@@ -81,11 +84,59 @@ class ChatHandler(WebSocketHandler, ABC):
 
 
 class SignupHandler(WebSocketHandler, ABC):
+    pools = dict()
+
+    def record_file_path(self) -> str:
+        teacherName = self.get_argument("teacherName")
+        courseName = self.get_argument("courseName")
+        return "hello.txt"
+        # return './resources//signup//%s//%s.txt' % \
+        #        (teacherName, courseName)
+
     def open(self):
-        pass
+        teacherName = self.get_argument("teacherName")
+        courseName = self.get_argument("courseName")
+        filepath = self.record_file_path()
+        with open(filepath, 'a', encoding='utf-8') as fp:
+            fp.write("%s\n" % datetime.today())
 
-    def on_message(self):
-        pass
+        if not self.pools.get(teacherName):
+            self.pools[teacherName] = dict()
+        if not self.pools[teacherName].get(courseName):
+            self.pools[teacherName][courseName] = Pool()
+            self.pools[teacherName][courseName].users = set()
+            self.pools[teacherName][courseName].user_dict = dict()
+        self.pools[teacherName][courseName].users.add(self)
+        self.pools[teacherName][courseName].user_dict[self] = self.get_cookie("username")
 
-    def on_close():
-        pass
+    def on_message(self, message):
+        teacherName = self.get_argument("teacherName")
+        courseName = self.get_argument("courseName")
+
+        message_arr = message.split(' ')
+        header = message_arr[0]
+        filepath = self.record_file_path()
+
+        if header == 'start-signup':
+            for user in self.pools[teacherName][courseName].users:
+                if user != self:
+                    user.write_message("start")
+        else:
+            fp = open(filepath, 'a', encoding='utf-8')
+            student = message_arr[1]
+            time = message_arr[2]
+            if header == 'confirm-signup':
+                fp.write("%s 已签到 %s" % (student, time))
+            elif header == 'refuse-signup':
+                fp.write("%s 未签到" % student)
+            else:
+                pass
+            fp.close()
+        
+    def on_close(self):
+        teacherName = self.get_argument("teacherName")
+        username = self.get_cookie("username")
+        if teacherName == username:
+            filepath = self.record_file_path()
+            with open(filepath, 'a', encoding='utf-8') as fp:
+                fp.write("\n");
